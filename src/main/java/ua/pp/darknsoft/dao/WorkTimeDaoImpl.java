cc1d0a76a67d935f017420cdf65038c8139c2370
Andrew <samsonov.omr@gmail.com>
2017-02-27 17:32:36 +0200

    My Work 27.02.2017

--- src/main/java/ua/pp/darknsoft/dao/WorkTimeDaoImpl.java
+++ src/main/java/ua/pp/darknsoft/dao/WorkTimeDaoImpl.java
@@ -3,10 +3,6 @@ package ua.pp.darknsoft.dao;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
-import ua.pp.darknsoft.dao.crud.user.DeleteUser;
-import ua.pp.darknsoft.dao.crud.user.InsertUser;
-import ua.pp.darknsoft.dao.crud.user.SelectUser;
-import ua.pp.darknsoft.dao.crud.user.UpdateUser;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
